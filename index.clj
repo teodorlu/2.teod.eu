c79971e22022ef74356c10b59d51c69b086f7552
Teodor Heggelund <git@teod.eu>
2024-02-03 17:06:39 +0100

    Get rid of the header

--- index.clj
+++ index.clj
@@ -22,8 +22,21 @@
 
 :trigger/rebuild
 
-{::clerk/visibility {:result :show}}
-
 (clerk/table [[1 2]
               [3 4]
               [5 6]])
+
+{::clerk/visibility {:result :show}}
+^{::clerk/width :full}
+(clerk/html
+ [:<>
+  [:style {:type "text/css"
+           ;; FIXME: this makes escaping in SSR happy
+           :dangerouslySetInnerHTML {:__html
+                                     "
+#clerk .notebook-viewer .viewer:first-child {
+  display: none;
+}
+"}}]
+  [:h1 "hi"]
+  [:p "hi hi "]])

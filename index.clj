3dedbfcbe14d6e8f6ab954250b2c0f5d1bcecffb
Teodor Heggelund <git@teod.eu>
2024-02-03 17:10:37 +0100

    Delete things we don't need

--- index.clj
+++ index.clj
@@ -3,24 +3,7 @@
    :nextjournal.clerk/open-graph
    {:url "https://2.teod.eu"}}
   (:require
-   [nextjournal.clerk :as clerk]
-   [nextjournal.clerk.view :as clerk.view]))
-
-(alter-var-root #'clerk.view/include-css+js
-                (fn [include-css+js-orig extra-includes]
-                  (fn [state]
-                    (concat (include-css+js-orig state)
-                            extra-includes)))
-                (list))
-
-
-2
-
-3
-
-:hello/world
-
-:trigger/rebuild
+   [nextjournal.clerk :as clerk]))
 
 (clerk/table [[1 2]
               [3 4]

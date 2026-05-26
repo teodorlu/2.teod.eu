4614f2a2c999418539e13d46751bc143b0de46e8
Teodor Heggelund <git@teod.eu>
2026-05-26 09:22:23 +0200

    Cleanup: build.clj

--- src/t2/build.clj
+++ src/t2/build.clj
@@ -2,12 +2,9 @@
 
 (comment
   (require '[t2.html :refer [html body pre]])
-  (def text (slurp "d/10/wax-and-wane.txt"))
-  (html (body (pre text)))
-
   (spit "d/10/wax-and-wane.html"
         (str
          "<!DOCTYPE html>\n"
-         (html (body (pre text)))))
+         (html (body (pre (slurp "d/10/wax-and-wane.txt"))))))
 
   )

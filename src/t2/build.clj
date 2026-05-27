75995eca75d7df6572f4c140c9def40289d65b8b
Teodor Heggelund <git@teod.eu>
2026-05-27 18:47:22 +0200

    Index.html: List docs

--- src/t2/build.clj
+++ src/t2/build.clj
@@ -3,16 +3,33 @@
             [clojure.string :as str]
             [babashka.fs :as fs]))
 
-(def dottxt->dothtml #(str/replace % #".txt$" ".html"))
-
-(defn txt->html [txt]
+(defn render-pre [txt]
   (str "<!DOCTYPE html>\n"
        (h/html (h/body (h/pre txt)))))
 
+(def dottxt->dothtml #(str/replace % #".txt$" ".html"))
+(def dottxt->slug #(str/replace % #".txt$" ""))
+
+(defn index [sources]
+  (str
+   "YO
+
+READ STUFF:\n"
+   (->> sources
+        (map #(str "  " (dottxt->slug %)))
+        (str/join "\n"))))
+
 (defn build [sources]
+  (spit "index.html" (render-pre (index sources)))
   (doseq [s sources]
     (spit (dottxt->dothtml s)
-          (txt->html (slurp s)))))
+          (render-pre (slurp s)))))
+
+(comment
+
+  (spit "index.html" (render-pre (index the-sources)))
+
+  )
 
 (defn clean [sources]
   (run! fs/delete-if-exists (map dottxt->dothtml sources)))

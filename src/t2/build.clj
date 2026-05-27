f068b4a5585d014b312b7f5a89ee58fb93f2575d
Teodor Heggelund <git@teod.eu>
2026-05-27 18:34:54 +0200

    Build: Split the build fn from its input

--- src/t2/build.clj
+++ src/t2/build.clj
@@ -1,10 +1,27 @@
-(ns t2.build)
+(ns t2.build
+  (:require [t2.html :as h]
+            [clojure.string :as str]
+            [babashka.fs :as fs]))
+
+(def dottxt->dothtml #(str/replace % #".txt$" ".html"))
+
+(defn txt->html [txt]
+  (str "<!DOCTYPE html>\n"
+       (h/html (h/body (h/pre txt)))))
+
+(defn build [sources]
+  (doseq [s sources]
+    (spit (dottxt->dothtml s)
+          (txt->html (slurp s)))))
+
+(defn clean [sources]
+  (run! fs/delete-if-exists (map dottxt->dothtml sources)))
+
+(def the-sources
+  (sorted-set "d/10/wax-and-wane.txt"))
 
 (comment
-  (require '[t2.html :refer [html body pre]])
-  (spit "d/10/wax-and-wane.html"
-        (str
-         "<!DOCTYPE html>\n"
-         (html (body (pre (slurp "d/10/wax-and-wane.txt"))))))
+  (build the-sources)
+  (clean the-sources)
 
   )

caa4d58aaf6608c9ad8308a9430be45292c232ce
Teodor Heggelund <git@teod.eu>
2026-07-23 10:59:19 +0200

    assetnow.checksum: add SHA256

--- src/assetnow/checksum.clj
+++ src/assetnow/checksum.clj
@@ -1,12 +1,18 @@
 (ns assetnow.checksum
+  (:require [babashka.fs :as fs])
   (:import [java.security MessageDigest]
-           [java.util HexFormat])
-  (:require [babashka.fs :as fs]))
+           [java.util HexFormat]))
 
-(defn bytes->md5-hexdigest [^bytes bytes]
+(defn bytes->hexdigest [algorithm ^bytes bytes]
   (.formatHex
    (HexFormat/of)
-   (.digest (MessageDigest/getInstance "MD5") bytes)))
+   (.digest (MessageDigest/getInstance algorithm) bytes)))
+
+(defn bytes->md5-hexdigest [^bytes bytes]
+  (bytes->hexdigest "MD5" bytes))
+
+(defn bytes->sha256-hexdigest [^bytes bytes]
+  (bytes->hexdigest "SHA-256" bytes))
 
 (defn stamp-path [raw-path checksum]
   (str (fs/strip-ext raw-path) "." checksum "." (fs/extension raw-path)))
--- test/assetnow/checksum_test.clj
+++ test/assetnow/checksum_test.clj
@@ -5,7 +5,12 @@
 (deftest bytes->md5-hexdigest-test
   (let [bytes (.getBytes "Call me Ishmael." "UTF-8")]
     (is (= "cbee6f8c54d146395a727b9097e5972a"
-           (checksum/bytes->md5-hexdigest bytes)))))
+            (checksum/bytes->md5-hexdigest bytes)))))
+
+(deftest bytes->sha256-hexdigest-test
+  (let [bytes (.getBytes "Call me Ishmael." "UTF-8")]
+    (is (= "458f3ceeeec730139693560ecf66c9c22d9c7bc7dcb0599e8e10b667dfeac043"
+           (checksum/bytes->sha256-hexdigest bytes)))))
 
 (deftest stamp-path
   (is (= "/moby-dick.cbee.txt"

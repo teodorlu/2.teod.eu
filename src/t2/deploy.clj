1743b70ca480d902f99cc1ee94349fd304b9f316
Teodor Heggelund <git@teod.eu>
2026-07-27 19:05:41 +0200

    Remove mentions of 3.teod.eu

--- src/t2/deploy.clj
+++ src/t2/deploy.clj
@@ -1,12 +1,11 @@
 (ns t2.deploy
   (:require [babashka.process]))
 
-(def targets ["root@t3:/var/www/2.teod.eu/"
-              "root@t3:/var/www/3.teod.eu/"])
+(def target "root@t3:/var/www/2.teod.eu/")
 (def include ["index.html" "d" "jtk" "css" "fonts"])
 (def exclude ["*.ttext" "*.ttex" "*.htm" ".DS_Store"])
 
-(defn deploy-argv [target]
+(defn deploy-argv []
   (into []
         cat
         [["rsync" "-a" "--delete" "--relative"]
@@ -14,12 +13,12 @@
          include
          [target]]))
 
-(defn deploy-dry-argv [target]
-  (conj (deploy-argv target) "-n" "-i"))
+(defn deploy-dry-argv []
+  (conj (deploy-argv) "-n" "-i"))
 
 (defn deploy []
-  (run! #(babashka.process/shell (deploy-argv %)) targets))
+  (babashka.process/shell (deploy-argv)))
 
 (defn deploy-dry []
-  (run! #(babashka.process/shell (deploy-dry-argv %)) targets))
+  (babashka.process/shell (deploy-dry-argv)))
 

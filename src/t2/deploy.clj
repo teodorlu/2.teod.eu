827508158c0da82a31883d46fbf0118bd848640b
Teodor Heggelund <git@teod.eu>
2026-07-27 19:01:57 +0200

    Deploy to Hetzner VPS

---
+++ src/t2/deploy.clj
@@ -0,0 +1,24 @@
+(ns t2.deploy
+  (:require [babashka.process]))
+
+(def target "root@t3:/var/www/3.teod.eu/")
+(def include ["index.html" "d" "jtk" "css" "fonts"])
+(def exclude ["*.ttext" "*.ttex" "*.htm" ".DS_Store"])
+
+(defn deploy-argv []
+  (into []
+        cat
+        [["rsync" "-a" "--delete" "--relative"]
+         (map #(str "--exclude=" %) exclude)
+         include
+         [target]]))
+
+(defn deploy-dry-argv []
+  (conj (deploy-argv) "-n" "-i"))
+
+(defn deploy []
+  (babashka.process/shell (deploy-argv)))
+
+(defn deploy-dry []
+  (babashka.process/shell (deploy-dry-argv)))
+

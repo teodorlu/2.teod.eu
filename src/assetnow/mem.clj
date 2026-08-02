b4fc0fe9b90936c7b2d1f02038ad1e59ac6da43e
Teodor Heggelund <git@teod.eu>
2026-08-02 10:32:17 +0200

    rebuild with global /css/t2.css

--- src/assetnow/mem.clj
+++ src/assetnow/mem.clj
@@ -10,7 +10,6 @@
         (let [checksum (checksum/bytes->md5-hexdigest bytes)
               stamped-path (checksum/stamp-path path checksum)]
           (swap! store assoc-in [:serve stamped-path] bytes)
-          ;;(swap! store assoc-in [:resolve-asset path] stamped-path)
           stamped-path))
 
       (list-assets [_]
@@ -21,15 +20,3 @@
       (respond [_ path]
         (when-let [bytes (get-in @store [:serve path])]
           {:body bytes})))))
-
-(comment
-  ;; resolve-all
-  ;; :resolve-asset
-
-  {:resolve-asset {"icecream.txt" "icecream.abc.txt"}
-   :asset-backlinks {"icecream.txt" ["icestory.txt" 1]}
-   :fragments {"icestory.txt" [(String/.getBytes "Everyone wants icecream." "UTF-8")
-                               "icecream.abc.txt"]}
-   :serve {"a.abc.txt" (String/.getBytes "Call me Icecream" "UTF-8")
-           "icestory.txt" (byte-array 0)}}
-  )

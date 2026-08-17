7109eabb3809b5e9cc16283a2ece5524051f0a5a
Teodor Heggelund <git@teod.eu>
2026-08-17 13:33:19 +0200

    Use assetnow repo as dependency source

--- src/assetnow/protocols.clj
+++
@@ -1,8 +0,0 @@
-(ns assetnow.protocols
-  (:require [assetnow.checksum :as checksum]))
-
-(defprotocol Store
-  (push-asset [this url asset-bytes])
-  (list-assets [this])
-  (push-fragments [this url fragments])
-  (respond [this url]))

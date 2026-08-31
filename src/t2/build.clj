383f21f4a0dfa5b189d70f40d74b59933c6075ee
Teodor Heggelund <git@teod.eu>
2026-08-31 11:08:33 +0200

    d/2a the simplicity ethos

--- src/t2/build.clj
+++ src/t2/build.clj
@@ -90,7 +90,7 @@
   (let [sources (into #{} (map (comp str d/find)) source-ids)]
     (run! fs/delete-if-exists (map dot-ttext->dot-html sources))))
 
-(def draft-ids #{"1c" "20" "21" "23" "2a"})
+(def draft-ids #{"1c" "20" "21" "23"})
 (def all-ids (set (d/all)))
 (def published-ids (set/difference all-ids draft-ids))
 
